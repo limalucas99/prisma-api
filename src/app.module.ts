e33a7ff4ec946178d87668db9becd7f66fd1a984
limalucas99 <lucaslim.lux@gmail.com>
2022-06-28 07:18:00 -0300

    Adding Prisma Cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot()],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
