641740da51d682ba4e0588c5a10fd204cb887f2d
limalucas99 <lucaslim.lux@gmail.com>
2022-07-01 11:14:30 -0300

    Tratamento de erros customizado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/not-found.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+// import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +16,11 @@ async function bootstrap() {
             transform: true,
         }),
     );
+    // app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(new ConflictInterceptor()); // A ordem dos interceptos importa
+    app.useGlobalInterceptors(new DatabaseInterceptor());
+    app.useGlobalInterceptors(new UnauthorizedInterceptor());
+    app.useGlobalInterceptors(new NotFoundInterceptor());
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
+// import { UnauthorizedError } from 'src/common/errors/types/UnauthorizedError';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
 import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
@@ -11,18 +14,23 @@ export class UsersService {
     }
 
     findAll() {
+        // throw new UnauthorizedError('Não autorizado!');
         return this.repository.findAll();
     }
 
-    findOne(id: number) {
-        return this.repository.findOne(id);
+    async findOne(id: number): Promise<UserEntity> {
+        const user = await this.repository.findOne(id);
+        if (!user) {
+            throw new NotFoundError('Usuario não encontrado');
+        }
+        return user;
     }
 
-    update(id: number, updateUserDto: UpdateUserDto) {
+    update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
         return this.repository.update(id, updateUserDto);
     }
 
-    remove(id: number) {
+    remove(id: number): Promise<UserEntity> {
         return this.repository.remove(id);
     }
 }
