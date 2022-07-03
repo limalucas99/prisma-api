dea6ad5a161939e5ecea14bf33c65d9d7f2b83ae
limalucas99 <lucaslim.lux@gmail.com>
2022-07-03 00:10:37 -0300

    Add swagger docs

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+    @ApiProperty({ description: 'Titulo do Post' })
     @IsString()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty({ description: 'Conteúdo do Post' })
     @IsString()
     @IsOptional()
     content?: string;
 
+    @ApiProperty({ description: 'Email do autor do Post' })
     @IsEmail()
     authorEmail: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,31 +10,39 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @ApiResponse({ status: 409, description: 'Conflito de Email' })
+    @ApiForbiddenResponse({ description: 'Acesso Negado' })
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
     }
 
+    @ApiForbiddenResponse({ description: 'Acesso Negado' })
     @Get()
     findAll() {
         return this.usersService.findAll();
     }
 
+    @ApiForbiddenResponse({ description: 'Acesso Negado' })
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.usersService.findOne(+id);
     }
 
+    @ApiForbiddenResponse({ description: 'Acesso Negado' })
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
         return this.usersService.update(+id, updateUserDto);
     }
 
+    @ApiForbiddenResponse({ description: 'Acesso Negado' })
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.usersService.remove(+id);
